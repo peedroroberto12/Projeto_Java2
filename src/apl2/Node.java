8ecb7a1644db064d77e4436d40890b09b69d6b66
peedroroberto12 <peedroroberto@gmail.com>
2025-05-14 01:21:37 +0000

    Arquivo Node completo

--- src/apl2/Node.java
+++ src/apl2/Node.java
@@ -1,7 +1,3 @@
-// arquivo: src/apl2/Node.java
-
-// TODO: Colocar a identificação dos(as) integrantes aqui.
-
 package apl2;
 
 // -- A classe Node (que pertence ao pacote apl2) deve conter os atributos que
@@ -15,8 +11,73 @@ package apl2;
 // {...}, retornando uma string com os valores dos atributos da classe.
 
 public class Node {
-	
-	// TODO: Implementar a classe conforme o enunciado da atividade Apl2.
-	
-	
+
+    private String id;
+    private String nome;
+    private float nota;
+    private Node next;
+    private Node left;
+
+    // Construtor padrão
+    public Node() {
+        this("", "", 0.0f, null, null);  
+    }
+
+    // Construtor com parâmetros
+    public Node(String id, String nome, float nota, Node next, Node left) {
+        this.id = id;
+        this.nome = nome;
+        this.nota = nota;
+        this.next = next;
+        this.left = left;
+    }
+
+    // Getters e Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getNota() {
+        return nota;
+    }
+
+    public void setNota(float nota) {
+        this.nota = nota;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    // Sobrescrevendo o método toString
+    @Override
+    public String toString() {
+        return "[dados: (" + id + "; " + nome + "; " + nota + ") | next: " + 
+                (next != null ? next.getId() : "null") + " | left: " + 
+                (left != null ? left.getId() : "null") + "]";
+    }
 }
